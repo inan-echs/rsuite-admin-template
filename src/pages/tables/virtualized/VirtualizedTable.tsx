416f1b15b3e66e5bf6303837ccda9c08debb8893
Elgius <114131653+Elgius@users.noreply.github.com>
2024-05-19 15:58:21 +0500

    RC candidtae

--- src/pages/tables/virtualized/VirtualizedTable.tsx
+++ src/pages/tables/virtualized/VirtualizedTable.tsx
@@ -21,22 +21,22 @@ interface Datatype {
 }
 
 const VirtualizedTable = () => {
-  const [customer, setCustomer] = useState<Datatype>();
+  const [customer, setCustomer] = useState<Datatype[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // const token = localStorage.getItem('token');
-    const token = process.env.REACT_APP_TEMP_JWT;
+    // const token = process.env.REACT_APP_TEMP_JWT || '';
 
-    if (!token) {
-      console.error('No jwt token, please reauthenticate');
-      return;
-    }
+    // if (!token) {
+    //   console.error('No jwt token, please reauthenticate');
+    //   return;
+    // }
     const requestOptions = {
       method: 'GET',
       headers: {
         'Content-type': 'application/json',
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjU1ZDZlODljLTEyZTctNDE3YS1hNDUzLWQwYTIwOGJkYzcxYyIsInRlbmFudElkIjoicG9zZGVtbyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkNhc2hpZXIiLCJleHAiOjE3MTU4ODQ2NjEsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDAvIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMC8ifQ.-ooa7fNh5l6v6Ec91VuZZLDVYQH_3RPnIMxRGBRnkYg`
       }
     };
     fetch('https://pos.echesconsultancy.com:10000/FbPos/ListCustomers', requestOptions)
