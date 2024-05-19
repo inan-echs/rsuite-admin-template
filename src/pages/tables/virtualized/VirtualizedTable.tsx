25e4412e0aa027fbd500f085cf3d268d25c8b9c7
Elgius <114131653+Elgius@users.noreply.github.com>
2024-05-19 17:04:01 +0500

    yeet

--- src/pages/tables/virtualized/VirtualizedTable.tsx
+++ src/pages/tables/virtualized/VirtualizedTable.tsx
@@ -12,52 +12,66 @@ const { getHeight } = DOMHelper;
 
 // config();
 
-interface Datatype {
-  CardCode: string;
-  CardName: string;
-  VatStatus: string;
-  Phone1: string;
-  CardType: string;
-  ListNum: string;
+interface Item {
+  itemCode: string;
+  itemType: number;
+  itemName: string;
+  itemDesc: string;
+  gstInclusivePrice: boolean;
+  price: number;
+  taxable: boolean;
+  onHand: number;
+  inStock: boolean;
+  discountable: boolean;
+  active: boolean;
+  itemCategoryId: number;
 }
 
 const VirtualizedTable = () => {
-  const [customer, setCustomer] = useState<Datatype[]>([]);
+  // const [customer, setCustomer] = useState<Datatype[]>([]);
+  const [tableData, setTableData] = useState<Item[]>([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // const token = localStorage.getItem('token');
-    // const token = process.env.REACT_APP_TEMP_JWT || '';
+  // bringing this back once the other stuff has been resolved
+  // useEffect(() => {
+  //   // const token = localStorage.getItem('token');
+  //   // const token = process.env.REACT_APP_TEMP_JWT || '';
 
-    // if (!token) {
-    //   console.error('No jwt token, please reauthenticate');
-    //   return;
-    // }
-    fetch({
-      url: 'https://pos.echesconsultancy.com:10000/FbPos/ListCustomers',
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json', // Corrected header name
-        Authorization: `Bearer "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjU1ZDZlODljLTEyZTctNDE3YS1hNDUzLWQwYTIwOGJkYzcxYyIsInRlbmFudElkIjoicG9zZGVtbyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkNhc2hpZXIiLCJleHAiOjE3MTYxNTk0NzQsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDAvIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMC8ifQ.Xn--YHlhXmYwRDiqJvVCtXtJTBd41NSI7lkxAoWXGFI"`,
-        userId: '55d6e89c-12e7-417a-a453-d0a208bdc71c',
-        name: ' '
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(fetchedData => {
-        setCustomer(fetchedData);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-        setLoading(false);
-      });
+  //   // if (!token) {
+  //   //   console.error('No jwt token, please reauthenticate');
+  //   //   return;
+  //   // }
+  //   fetch({
+  //     url: 'https://pos.echesconsultancy.com:10000/FbPos/ListCustomers',
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json', // Corrected header name
+  //       Authorization: `Bearer "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjU1ZDZlODljLTEyZTctNDE3YS1hNDUzLWQwYTIwOGJkYzcxYyIsInRlbmFudElkIjoicG9zZGVtbyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkNhc2hpZXIiLCJleHAiOjE3MTYxNTk0NzQsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDAvIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMC8ifQ.Xn--YHlhXmYwRDiqJvVCtXtJTBd41NSI7lkxAoWXGFI"`,
+  //       userId: '55d6e89c-12e7-417a-a453-d0a208bdc71c',
+  //       name: ' '
+  //     }
+  //   })
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(fetchedData => {
+  //       setCustomer(fetchedData);
+  //       setLoading(false);
+  //     })
+  //     .catch(error => {
+  //       console.error('There has been a problem with your fetch operation:', error);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    setTableData(items);
+    setLoading(false);
   }, []);
+
   return (
     <>
       {loading ? (
@@ -66,43 +80,40 @@ const VirtualizedTable = () => {
         <Table
           virtualized
           height={Math.max(getHeight(window) - 120, 400)}
-          data={customer}
+          data={tableData}
           translate3d={false}
         >
           <Column width={70} align="center" fixed>
-            <HeaderCell>Card Id</HeaderCell>
-            <Cell dataKey="id" />
+            <HeaderCell>Item Code</HeaderCell>
+            <Cell dataKey="itemCode" />
           </Column>
 
           <Column width={130}>
-            <HeaderCell>Name</HeaderCell>
-            <Cell dataKey="firstName" />
+            <HeaderCell>Item Name</HeaderCell>
+            <Cell dataKey="itemName" />
           </Column>
 
           <Column width={130}>
-            <HeaderCell>Vat Status</HeaderCell>
-            <Cell dataKey="lastName" />
+            <HeaderCell>Description</HeaderCell>
+            <Cell dataKey="itemDesc" />
           </Column>
 
           <Column width={100}>
-            <HeaderCell>Card type</HeaderCell>
-            <Cell dataKey="gender" />
+            <HeaderCell>Taxable</HeaderCell>
+            <Cell dataKey="taxable" />
           </Column>
 
           <Column width={100}>
-            <HeaderCell>List num</HeaderCell>
-            <Cell dataKey="age" />
+            <HeaderCell>In Stock</HeaderCell>
+            <Cell dataKey="inStock" />
           </Column>
 
           <Column width={200}>
-            <HeaderCell>Address</HeaderCell>
-            <Cell dataKey="city" />
+            <HeaderCell>On Hand</HeaderCell>
+            <Cell dataKey="onHand" />
           </Column>
 
-          <Column minWidth={200} flexGrow={1}>
-            <HeaderCell>Email</HeaderCell>
-            <Cell dataKey="email" />
-          </Column>
+          {/* Add more columns as needed */}
         </Table>
       )}
     </>
